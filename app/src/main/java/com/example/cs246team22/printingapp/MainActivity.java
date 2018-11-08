c18fada147e550e57c6bf6798d496363f1184777
aidencompsci <aidenreid55@gmail.com>
2018-11-07 18:07:28 -0700

    testing if commits work, I think I have everything set up right

--- app/src/main/java/com/example/cs246team22/printingapp/MainActivity.java
+++ app/src/main/java/com/example/cs246team22/printingapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //aiden testing commits
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
