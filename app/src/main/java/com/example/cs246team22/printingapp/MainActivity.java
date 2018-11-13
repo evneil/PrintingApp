37f72c6957f65157fa5ce6afa2a938431fb69333
evneil <eddie.neil@gmail.com>
2018-11-12 19:38:59 -0500

    Test

--- app/src/main/java/com/example/cs246team22/printingapp/MainActivity.java
+++ app/src/main/java/com/example/cs246team22/printingapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //aiden testing commits
-
+    //Edward Testing Commits
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
