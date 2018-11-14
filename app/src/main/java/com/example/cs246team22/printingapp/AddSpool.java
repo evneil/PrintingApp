4c82190c076b82cd6d5b9124e413e1927bd2f2af
evneil <eddie.neil@gmail.com>
2018-11-14 12:40:55 -0500

    Test

---
+++ app/src/main/java/com/example/cs246team22/printingapp/AddSpool.java
@@ -0,0 +1,13 @@
+package com.example.cs246team22.printingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSpool extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_spool);
+    }
+}
