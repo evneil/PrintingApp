a5fd6bdc5dfb5257627a69d007214e591b656175
evneil <eddie.neil@gmail.com>
2018-11-15 21:19:16 -0500

    Add Empty Acivity

---
+++ app/src/main/java/com/example/cs246team22/printingapp/AddSpoolActivity.java
@@ -0,0 +1,12 @@
+package com.example.cs246team22.printingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSpoolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
